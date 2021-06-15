ad1deeb4fc03d47728bf640a06724b7221d63698
Natasha Carr <74566670+tash-ndc@users.noreply.github.com>
2021-06-15 21:42:22 +0100

    created random card and edited drink card.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import DrinkCard from './Components/DrinkCard/DrinkCard';
+import RandomCard from './Components/RandomCard/RandomCard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <DrinkCard />
+      <br /><br />
+      <RandomCard />
+      <br /><br />
     </div>
   );
 }
---
+++ src/Components/RandomCard/RandomCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../RandomCard/RandomCard.css';
+import testImg from './test_img.jpg';
+
+const RandomCard = () => {
+    return (
+        <div className='random-card'>
+            <img className='random-drink-img' src={testImg} alt='test drink' />
+            <h1 className='random-drink-name'>Drink Name</h1>
+            <h2 className='ingrediants-title drink-info'>Ingrediants:</h2>
+            <ul className='ingrediants-list drink-info'>
+                <li>Ingrediant 1</li>
+                <li>Ingrediant 2</li>
+                <li>Ingrediant 3</li>
+            </ul>
+            <h2 className='directions-title drink-info'>Directions:</h2>
+            <p className='directions drink-info'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sem risus, finibus ac pretium vitae, auctor et justo. Ut fermentum.
+
+</p>
+        </div>
+    )
+}
+
+export default RandomCard;
