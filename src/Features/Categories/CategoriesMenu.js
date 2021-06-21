c33cae26ec4acfe2671c076e36984a442eadc12a
Natasha Carr <74566670+tash-ndc@users.noreply.github.com>
2021-06-21 13:36:47 +0100

    nav tweaks, category menu complete.

--- src/Features/Categories/CategoriesMenu.js
+++ src/Features/Categories/CategoriesMenu.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import '../Categories/CategoriesMenu.css';
-import CategoryCard from '../../Components/CategoryCard/CategoryCard';
 
 const CategoriesMenu = () => {
     return (
         <div className='categories-menu'>
-            <CategoryCard />
+            <div className='cocktails'>
+                <button className='cocktails-btn'>cocktails</button>
+            </div>
+            <div className='prem-cocktails'>
+                <button className='prem-cocktails-btn'>premium cocktails</button>
+            </div>
+            <div className='party'>
+                <button className='party-btn'>party</button>
+            </div>
+            <div className='shots'>
+                <button className='shots-btn'>shots</button>
+            </div>
+            <div className='homemade-liqueur'>
+                <button className='homemade-liqueur-btn'>homemade liqueur</button>
+            </div>
+            <div className='beer'>
+                <button className='beer-btn'>beer</button>
+            </div>
         </div>
     );
 }
